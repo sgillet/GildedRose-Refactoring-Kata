e5a3a8b9c05891044c59514bfd69f5d1c85c255c
Sebastien Gillet <sbsgillet@gmail.com>
2018-04-08 07:13:19 +0200

    feat: reduce ifs & change switch to .map

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -16,63 +16,50 @@ class Shop {
     if(brie.quality < 50) {
       brie.quality += 1;
     }
-    if (brie.sellIn < 0) {
-      if (brie.quality < 50) {
-        brie.quality += 1;
-      }
+    if (brie.sellIn < 0 && brie.quality < 50) {
+      brie.quality += 1;
     }
   }
   updateBackstageQuality(backstage) {
     if (backstage.quality < 50) {
       backstage.quality += 1;
-      if (backstage.sellIn < 11) {
-        if (backstage.quality < 50) {
-          backstage.quality += 1;
-        }
+      if (backstage.sellIn < 11 && backstage.quality < 50) {
+        backstage.quality += 1;
       }
-      if (backstage.sellIn < 6) {
-        if (backstage.quality < 50) {
-          backstage.quality += 1;
-        }
+      if (backstage.sellIn < 6 && backstage.quality < 50) {
+        backstage.quality += 1;
       }
+    }
+    if (backstage.sellIn < 1) {
+      backstage.quality = 0;
     }
     backstage.sellIn = backstage.sellIn - 1;
-    if (backstage.sellIn < 0) {
-      backstage.quality = backstage.quality - backstage.quality;
-    }
-  }
-  updateSulfurasQuality(sulfuras) {
   }
   updateDefaultItemQuality(item) {
     item.sellIn = item.sellIn - 1;
     if (item.quality > 0) {
       item.quality = item.quality - 1;
     }
-
-    if (item.sellIn < 0) {
-      if (item.quality > 0) {
-          item.quality = item.quality - 1;
-        }
-      }
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality = item.quality - 1;
+    }
   }
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      switch (this.items[i].name) {
+    this.items.map((item) => {
+      switch (item.name) {
         case 'Aged Brie':
-          this.updateAgedBrieQuality(this.items[i]);
+          this.updateAgedBrieQuality(item);
           break;
         case 'Backstage passes to a TAFKAL80ETC concert':
-          this.updateBackstageQuality(this.items[i]);
+          this.updateBackstageQuality(item);
           break;
         case 'Sulfuras, Hand of Ragnaros':
-          this.updateSulfurasQuality(this.items[i]);
           break;
         default:
-          this.updateDefaultItemQuality(this.items[i]);
+          this.updateDefaultItemQuality(item);
           break;
       }
-    }
-
+    });
     return this.items;
   }
 }
